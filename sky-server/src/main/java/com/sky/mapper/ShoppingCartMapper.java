bc1daa09093cba28aa47ff06a7c42b8a746d4d4d
LMHY-hjm <luemenghanyi@gmail.com>
2026-04-22 17:39:18 +0800

    地址簿功能代码

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -38,5 +38,12 @@ public interface ShoppingCartMapper {
      * @param userId
      */
     @Delete("delete from shopping_cart where user_id = #{userId}")
-    void deleteById(Long userId);
+    void deleteByUserId(Long userId);
+
+    /**
+     * 根据id删除购物车数据
+     * @param id
+     */
+    @Delete("delete from shopping_cart where id = #{id}")
+    void deleteById(Long id);
 }
