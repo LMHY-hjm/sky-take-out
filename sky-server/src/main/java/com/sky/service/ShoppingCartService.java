bfa452374621b6a54b2b850722d5502267d27bc7
LMHY-hjm <luemenghanyi@gmail.com>
2026-04-22 14:44:06 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -3,7 +3,18 @@ package com.sky.service;
 import com.sky.dto.ShoppingCartDTO;
 import com.sky.entity.ShoppingCart;
 
-public interface ShoppingCartService {
+import java.util.List;
 
+public interface ShoppingCartService {
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    List<ShoppingCart> showShoppingCart();
 }
