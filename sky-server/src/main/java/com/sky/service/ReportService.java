c7a9aefa002b84cb63c0371c623e6aac8c83d78a
LMHY-hjm <luemenghanyi@gmail.com>
2026-05-04 17:30:24 +0800

    用户数据统计功能代码开发

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,16 +1,25 @@
 package com.sky.service;
 
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 
 import java.time.LocalDate;
 
 public interface ReportService {
 
     /**
-     * 统计置顶时间区间内的营业额数据
+     * 统计指定 时间区间内的营业额数据
      * @param begin
      * @param end
      * @return
      */
     TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
+
+    /**
+     * 统计指定时间区间内的用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
 }
